53d3db9f7e9c05583e2a39100c852bca1cb86d70
lucaoskaique <lucas.kaique175@hotmail.com>
2024-10-15 11:02:21 -0300

    Add functions for calculating reading time and returning unique values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// a function to calculate reading time
+export const timeToRead = (text: string) => {
+  const words = text.split(" ")
+  const minutes = Math.ceil(words.length / 200)
+  return `${minutes} min de leitura`
+}
+
+// a function to return only unique values
+export const unique = <T>(val: T, index: number, self: T[]): boolean => {
+  return self.indexOf(val) === index
+}
