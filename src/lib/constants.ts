2ef9970cbd5c9647f2679c718c8f80b432840c80
lucaoskaique <lucas.kaique175@hotmail.com>
2024-10-15 11:01:48 -0300

    Add constants for blog title, subtitle, description, author, author link, URL, author position, and author description

---
+++ src/lib/constants.ts
@@ -0,0 +1,17 @@
+export const BLOG_TITLE = "Lucas Kaíque"
+
+export const BLOG_SUBTITLE =
+  "Um blog de um Engenheiro de Software, Produtor de Eventos, DJ no tempo livre e fala de Javascript, React e outras tecnologias."
+
+export const BLOG_DESCRIPTION = [BLOG_TITLE, BLOG_SUBTITLE].join(" | ")
+
+export const BLOG_AUTHOR = "Lucas Kaíque"
+
+export const BLOG_AUTHOR_LINK = "https://x.com/lucaoskaique"
+
+export const BLOG_URL = "https://lucaskaique.com.br"
+
+export const BLOG_AUTHOR_POSITION = "Software Engineer"
+
+export const BLOG_AUTHOR_DESCRIPTION =
+  "Engenheiro de Software e DJ no tempo livre, exploro o mundo da tecnologia e a intersecção entre música, arte e programação. Escrevo sobre estas e outras experiências instigantes."
