648f43118a4a0cfce20f2eb9a796b68699f0fd50
lucaoskaique <lucas.kaique175@hotmail.com>
2024-10-15 11:01:35 -0300

    Add API functions to retrieve and process post data

---
+++ src/lib/api.ts
@@ -0,0 +1,51 @@
+import { format } from "date-fns"
+import { pt } from "date-fns/locale"
+import fs from "fs"
+import matter from "gray-matter"
+import { join } from "path"
+
+import { Post } from "@/types"
+
+const postsDirectory = join(process.cwd(), "posts")
+
+export function getPostBySlug(slug: string): Post | null {
+  if (!slug) return null
+
+  try {
+    const realSlug = slug.replace(/\.md$/, "")
+    const fullPath = join(postsDirectory, `${realSlug}.md`)
+    const fileContents = fs.readFileSync(fullPath, "utf8")
+    const { data, content } = matter(fileContents)
+
+    const postData = data as Post["frontmatter"]
+
+    const date = format(new Date(postData.date), "dd 'de' MMMM 'de' yyyy", {
+      locale: pt
+    })
+
+    return {
+      slug: realSlug,
+      date: postData.date.toString(),
+      frontmatter: {
+        ...postData,
+        date
+      },
+      content
+    }
+  } catch (error) {
+    console.error("Error reading the file:", error)
+    return null
+  }
+}
+
+export function getAllPosts(): Post[] {
+  const slugs = fs.readdirSync(postsDirectory)
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug.replace(/\.md$/, "")))
+    .filter((post): post is Post => post !== null)
+    .sort((post1, post2) =>
+      new Date(post2.date) > new Date(post1.date) ? 1 : -1
+    )
+
+  return posts
+}
