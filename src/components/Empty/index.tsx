6980717882951ab80004c11398d768dc73eac9a6
lucaoskaique <lucas.kaique175@hotmail.com>
2024-10-17 17:39:06 -0300

    Refactor Empty component to use separate inner and outer containers

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -8,12 +8,21 @@ export type EmptyProps = {
   hasLink?: boolean
 }
 const Empty = ({ title, description, hasLink }: EmptyProps) => (
-  <div>
-    <h2>{title}</h2>
-    <p>{description}</p>
+  <div className="flex flex-col items-center">
+    <p className="text-base font-semibold text-zinc-400 dark:text-zinc-500">
+      404
+    </p>
+    <h1 className="mt-4 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+      {title}
+    </h1>
+    <p className="mt-4 text-base text-zinc-600 dark:text-zinc-400">
+      {description}
+    </p>
     {hasLink && (
-      <Link href="/" passHref>
-        <Button as="a">Go back to Home</Button>
+      <Link href="/" passHref className="mt-8">
+        <Button as="a" background="black">
+          Go back to Home
+        </Button>
       </Link>
     )}
   </div>
