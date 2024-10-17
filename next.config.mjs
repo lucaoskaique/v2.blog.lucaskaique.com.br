2240cbec7003851a4b4c51981bf955091f64ada2
lucaoskaique <lucas.kaique175@hotmail.com>
2024-10-17 19:30:44 -0300

    Add remote image patterns to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        port: "",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**"
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
