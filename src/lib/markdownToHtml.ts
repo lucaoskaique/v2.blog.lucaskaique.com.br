b4559afe6f8bffe9df03c9eac88390c45d1ed7f2
lucaoskaique <lucas.kaique175@hotmail.com>
2024-10-17 17:39:37 -0300

    Refactor date formatting in getPostBySlug function and remove unused imports
    - Removed the unused imports of "format" and "pt" from the "date-fns" library in the "api.ts" file.
    - Updated the date formatting logic in the "getPostBySlug" function to use the "toString" method instead of the "format" function.
    
    Update markdownToHtml function to support additional features
    
    - Added the "remark-gfm" plugin to support GitHub Flavored Markdown in the "markdownToHtml.ts" file.
    - Added the "rehype-prism" plugin with the "line-numbers" option to enable syntax highlighting with line numbers.

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,5 +1,7 @@
+import rehypePrism from "rehype-prism"
 import { remark } from "remark"
 import headings from "remark-autolink-headings"
+import remarkGfm from "remark-gfm"
 import html from "remark-html"
 import remarkOembed from "remark-oembed"
 import slug from "remark-slug"
@@ -7,6 +9,8 @@ import slug from "remark-slug"
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(html)
+    .use(remarkGfm)
+    .use(rehypePrism, { plugins: ["line-numbers"] })
     .use(remarkOembed as never)
     .use(slug as never)
     .use(headings as never, {
