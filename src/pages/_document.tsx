57d18b5a108bdbdc5a15b80d70cf2802964fdb07
lucaoskaique <lucas.kaique175@hotmail.com>
2024-10-17 17:39:52 -0300

    Refactor _document.tsx to add full height and antialiased class to the HTML element and update body styling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,13 @@ import { Head, Html, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html lang="pt-BR">
+    <Html lang="pt-BR" className="h-full antialiased">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
+      <body className="h-full bg-zinc-50 dark:bg-black">
+        <div className="">
+          <Main />
+          <NextScript />
+        </div>
       </body>
     </Html>
   )
