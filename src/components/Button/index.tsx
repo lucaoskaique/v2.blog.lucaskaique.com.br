c06cc50f2af59325c172e5d77e75853f4eef836b
lucaoskaique <lucas.kaique175@hotmail.com>
2024-10-20 18:40:02 -0300

    fix button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,94 +1,35 @@
-import {
-  AnchorHTMLAttributes,
-  ButtonHTMLAttributes,
-  ElementType,
-  forwardRef,
-  ForwardRefRenderFunction,
-  ReactNode
-} from "react"
+import clsx from "clsx"
+import Link from "next/link"
+import { ComponentPropsWithoutRef } from "react"
 
-import { cn } from "@/lib/utils"
-
-type ButtonTypes =
-  | AnchorHTMLAttributes<HTMLAnchorElement>
-  | ButtonHTMLAttributes<HTMLButtonElement>
-
-export type ButtonProps = {
-  background?: "white" | "black" | "red" | "transparent" | "secondary"
-  size?: "xlarge" | "large" | "medium" | "small"
-  icon?: ReactNode
-  fullWidth?: boolean
-  fontBold?: "bold" | "regular"
-  textColor?: "primary" | "secondary" | "white" | "black" | "red"
-  as?: ElementType
-  className?: string
-} & ButtonTypes
-
-const Button: ForwardRefRenderFunction<
-  HTMLAnchorElement | HTMLButtonElement,
-  ButtonProps
-> = (
-  {
-    children,
-    size = "large",
-    background = "red",
-    fullWidth = false,
-    icon,
-    textColor = "primary",
-    fontBold = "bold",
-    as: Component = "button",
-    className,
-    ...props
-  },
-  ref
-) => {
-  const sizeClasses = {
-    xlarge: "px-6 py-3 text-xl",
-    large: "px-4 py-2 text-lg",
-    medium: "px-2 py-1",
-    small: "px-1 py-[0.125rem] text-sm"
-  }
-
-  const backgroundClasses = {
-    red: "bg-highlight",
-    white: "bg-white",
-    black: "bg-black",
-    transparent: "bg-transparent",
-    secondary: "bg-background"
-  }
-
-  const textColorClasses = {
-    primary: "text-primary",
-    secondary: "text-secondary",
-    white: "text-white",
-    black: "text-black",
-    red: "text-highlight"
-  }
-
-  const fontWeightClasses = {
-    bold: "font-bold",
-    regular: "font-medium"
-  }
-
-  return (
-    <Component
-      className={cn(
-        "px- rounded-md tracking-wide",
-        fontWeightClasses[fontBold],
-        sizeClasses[size],
-        backgroundClasses[background],
-        textColorClasses[textColor],
-        background === "red" ? "text-white" : "",
-        fullWidth && "w-full",
-        icon && "flex items-center justify-center",
-        className
-      )}
-      ref={ref}
-      {...props}>
-      {icon && <span className="mr-2">{icon}</span>}
-      {children && <span>{children}</span>}
-    </Component>
-  )
+const variantStyles = {
+  primary:
+    "bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70",
+  secondary:
+    "bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70"
 }
 
-export default forwardRef(Button)
+type ButtonProps = {
+  variant?: keyof typeof variantStyles
+} & (
+  | (ComponentPropsWithoutRef<"button"> & { href?: undefined })
+  | ComponentPropsWithoutRef<typeof Link>
+)
+
+export function Button({
+  variant = "primary",
+  className,
+  ...props
+}: ButtonProps) {
+  className = clsx(
+    "inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none",
+    variantStyles[variant],
+    className
+  )
+
+  return typeof props.href === "undefined" ? (
+    <button className={className} {...props} />
+  ) : (
+    <Link className={className} {...props} />
+  )
+}
