e87948359aa46ce6757dc0b45d50798dfaa636f3
lucaoskaique <lucas.kaique175@hotmail.com>
2026-02-08 22:28:21 -0300

    chore: update project name and dependencies
    - Changed project name from "resgatemais" to "v2.lucaoskaique.com.br".
    - Updated "next-themes" dependency from version "^0.3.0" to "^0.4.6".
    - Upgraded "react" and "react-dom" from version "^18" to "^19.2.4".
    - Removed unused dependency "@mapbox/rehype-prism".
    - Adjusted import statement in theme provider for cleaner code.
    - Fixed CSS import order in globals.css to ensure proper styling.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: '/home/lucaos/projects/v2.blog.lucaskaique.com.br',
+  outputFileTracingRoot: __dirname,
   images: {
     qualities: [50, 75, 90, 100],
     remotePatterns: [
